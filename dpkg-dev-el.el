c4477dcb3cc732b160755376bbdabcfa7e022cae
Nicholas D Steeves <nsteeves@gmail.com>
2018-10-21 18:58:16 -0400

    Fix Commentary section in dpkg-dev-el.el.

--- dpkg-dev-el.el
+++ dpkg-dev-el.el
@@ -1,8 +1,13 @@
 ;;; dpkg-dev-el.el --- startup file for the debian-el package
 
-;;; Commentary:
-;; 
 
+;;; Commentary:
+;; This file sets up customisation groups, adds appropriate modes to
+;; auto-mode-alist, and forces UTF8 encoding to prevent user error due
+;; to misconfiguration of Emacs.  Dpkg-dev-el can be customised from
+;; the group convenience -> dpkg-dev-el, or directly from the
+;; dpkg-dev-el group.
+;;
 ;;; History:
 ;;
 ;; 2003-11-03 - Peter Galbraith
