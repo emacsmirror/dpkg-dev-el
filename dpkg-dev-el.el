a80f8ac5d81720cce90cf3bc5fbb45d50b1953d7
Nicholas D Steeves <nsteeves@gmail.com>
2018-10-21 20:08:45 -0400

    Make GPL-2+ licensing explicit in dpkg-dev-el.el header.

--- dpkg-dev-el.el
+++ dpkg-dev-el.el
@@ -5,6 +5,23 @@
 ;; Author: Peter S Galbraith <psg@debian.org>
 ;; Created: 2003-11-03
 
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; debian-copyright.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with your Debian installation, in /usr/share/common-licenses/GPL
+;; If not, write to the Free Software Foundation, 675 Mass Ave,
+;; Cambridge, MA 02139, USA.
+
 ;;; Commentary:
 ;; This file sets up customisation groups, adds appropriate modes to
 ;; auto-mode-alist, and forces UTF8 encoding to prevent user error due
