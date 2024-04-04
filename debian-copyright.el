f582d9ce0e2fb88d4a5fc7666ebfc1ab5de28986
Xiyue Deng <manphiz@gmail.com>
2024-04-04 16:01:16 -0700

    Improve email font lock in debian-copyright-mode
    * Don't fontify the enclosing `<>' for email addresses.

--- debian-copyright.el
+++ debian-copyright.el
@@ -117,8 +117,8 @@ additionally detects the extra `<' and `>' around the address.  I
 hope this works well enough until the day we have to follow
 https://stackoverflow.com/a/201378."
   (add-to-list 'debian-copyright-font-lock-keywords
-               '("<?[[:alnum:]._~-]+@[[:alnum:]._~-]+\\.[[:alnum:]._~-]+>?" .
-                 font-lock-variable-name-face)))
+               '("<?\\([[:alnum:]._~-]+@[[:alnum:]._~-]+\\.[[:alnum:]._~-]+\\)>?"
+                 (1 font-lock-variable-name-face))))
 
 (defun debian-copyright--font-lock-add-urls (protocol-prefixes)
   "Add font lock for a URL with PROTOCOL-PREFIXES."
