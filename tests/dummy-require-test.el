f107d8980972e22b2d0656a7a158f9fc8552e648
Xiyue Deng <manphiz@gmail.com>
2024-07-27 22:12:32 -0700

    dpkg-dev-el (37.16) unstable; urgency=medium
      [ Xiyue Deng ]
      * Add Homepage in d/control
      * Add Source in d/copyright
      * Drop explicit dependency on elpa-debian-el in favor of
        Package-Requires
      * Add optional version support to `reassign' (Closes: #503108)
      * Rename forward paragraph helper function using the internal function
        convention
      * Implement toggling team upload (Closes: #595067)
        - Also bind to "C-c C-t"
      * Fix fontification for frozen to properly detect proposed updates
      * Improve fontification of backports to only detect stable and oldstable
      * Add fontification for known release code names
        - Currently includes Debian and Ubuntu release code names
      * Add font lock for emails in debian-control-mode
      * Add font lock regexp for URLs in debian-control-mode
      * Improve email and url detection regexps
    
      [ Sven Joachim ]
      * Use run-mode-hooks rather than run-hooks (Closes: #1074305)
    
    [dgit import package dpkg-dev-el 37.16]

---
+++ tests/dummy-require-test.el
@@ -0,0 +1,14 @@
+(require 'debian-bts-control)
+(require 'debian-changelog-mode)
+(require 'debian-control-mode)
+(require 'debian-copyright)
+(require 'dpkg-dev-el)
+(require 'readme-debian)
+
+(ert-deftest dummy-require-test ()
+  "A dummy test to ensure all add-ons are byte-compiled.
+
+This helps catch bugs and dependency issues when developing
+without real unit tests.  When real tests are available please
+feel free to remove this one."
+  t)
