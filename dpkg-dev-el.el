1660bb01b69864988d60637e05cdbdf6d145be1d
Xiyue Deng <manphiz@gmail.com>
2025-03-23 03:09:51 -0700

    Bump version post release

--- dpkg-dev-el.el
+++ dpkg-dev-el.el
@@ -3,7 +3,7 @@
 ;; Copyright: 2003-2014 Peter S Galbraith
 
 ;; Author: Peter S Galbraith <psg@debian.org>
-;; Version: 37.18
+;; Version: 37.19-git
 ;; Package-Requires: ((emacs "27.1") (debian-el "37.0"))
 ;; Created: 2003-11-03
 
