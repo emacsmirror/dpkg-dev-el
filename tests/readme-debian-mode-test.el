e1ac89e121d944d2e67ac4655bfd6e7440167d51
Xiyue Deng <manphiz@gmail.com>
2025-03-24 00:03:34 -0700

    Add simple faceup tests for readme-debian-mode

---
+++ tests/readme-debian-mode-test.el
@@ -0,0 +1,17 @@
+(require 'readme-debian)
+(require 'ert)
+(require 'faceup)
+
+(defvar readme-debian-mode-font-lock-test-dir (faceup-this-file-directory))
+
+(defun readme-debian-mode-font-lock-test-apps (file)
+  "Test FILE is fontified as the corresponding .facup file describes."
+  (faceup-test-font-lock-file 'readme-debian-mode
+                              (concat readme-debian-mode-font-lock-test-dir
+                                      file)))
+(faceup-defexplainer readme-debian-mode-font-lock-test-apps)
+
+(ert-deftest readme-debian-mode-font-test ()
+  "faceup tests for readme-debian-mode."
+  (should (readme-debian-mode-font-lock-test-apps
+           "faceup/readme-debian-mode/simple-readme-debian")))
