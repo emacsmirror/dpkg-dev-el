b2890acb1e1c16b76313ff8469f01912a9a3f2c4
Xiyue Deng <manphiz@gmail.com>
2025-03-23 22:15:21 -0700

    Add simple faceup tests for debian-changelog-mode

--- tests/debian-changelog-mode-test.el
+++ tests/debian-changelog-mode-test.el
@@ -1,4 +1,20 @@
 (require 'debian-changelog-mode)
+(require 'ert)
+(require 'faceup)
+
+(defvar debian-changelog-mode-font-lock-test-dir (faceup-this-file-directory))
+
+(defun debian-changelog-mode-font-lock-test-apps (file)
+  "Test FILE is fontified as the corresponding .facup file describes."
+  (faceup-test-font-lock-file 'debian-changelog-mode
+                              (concat debian-changelog-mode-font-lock-test-dir
+                                      file)))
+(faceup-defexplainer debian-changelog-mode-font-lock-test-apps)
+
+(ert-deftest debian-changelog-mode-font-test ()
+  "faceup tests for debian-changelog-mode."
+  (should (debian-changelog-mode-font-lock-test-apps
+           "faceup/debian-changelog-mode/simple-changelog")))
 
 (ert-deftest debian-changelog-mode-fill-paragraph-test ()
   "Test reflowing a debian changelog file with erts files."
