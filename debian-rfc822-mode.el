449665831b675921d9948fdc2894d0833658546f
Xiyue Deng <manphiz@gmail.com>
2026-07-10 02:07:54 -0700

    Support multiline highlighting
    - Now sub-items are highlighted as long as they are under the same
    paragraph of their field, even on different continuation lines.

--- debian-rfc822-mode.el
+++ debian-rfc822-mode.el
@@ -91,28 +91,108 @@ A derived mode should customize this variable accordingly.")
   '("^[[:space:]]*\\(#.*\\)$" (1 font-lock-comment-face))
   "The font lock spec for comments.")
 
+(defun debian-rfc822-mode--continuation-line-p ()
+  "Non-nil if point is at the start of an RFC822 continuation line.
+A continuation line starts with whiltespace, but an empty line doesn't
+count."
+  (and (looking-at "[ \t]")
+       (not (looking-at "[ \t]*$"))))
+
+(defun debian-rfc822-mode--paragraph-bounds ()
+  "Return (beg . end) of a RFC822 paragraph.
+`beg' is the start of the field line, `end' is the position right after
+the last continuation line (lines with a space at the beginning), or
+right after the line containing the `beg' if there is no continuation
+line."
+  (let (beg end)
+    (save-excursion
+      (beginning-of-line)
+      (while (and (debian-rfc822-mode--continuation-line-p)
+                  (not (bobp)))
+        (forward-line -1))
+      (setq beg (point)))
+    (save-excursion
+      (beginning-of-line)
+      (forward-line 1)
+      (while (and (not (eobp))
+                  (debian-rfc822-mode--continuation-line-p))
+        (forward-line 1))
+      (setq end (point)))
+    (cons beg end)))
+
+(defun debian-rfc822-mode--highlight-extend-region ()
+  "Extend `font-lock-beg'/`font-lock-end' to the whole RFC822 paragraph.
+This is meant to be added to `font-lock-extend-region-functions'."
+  (let (changed)
+    (goto-char font-lock-beg)
+    (let ((beg (car (debian-rfc822-mode--paragraph-bounds))))
+      (when (< beg font-lock-beg)
+        (setq font-lock-beg beg
+              changed t)))
+    (goto-char (max (point-min) (1- font-lock-end)))
+    (let ((end (cdr (debian-rfc822-mode--paragraph-bounds))))
+      (when (> end font-lock-end)
+        (setq font-lock-end end
+              changed t)))
+    changed))
+
+(defvar-local debian-rfc822-mode--conditional-field nil
+  "The field name being matched for highlighting.
+This is used internally, set by `debian-rfc822-mode--pre-match-form',
+and used by `debian=rfc822-mode--anchor-matcher'.")
+
+(defun debian-rfc822-mode--pre-match-form ()
+  "Compute the search bound for the anchored keyword matcher.
+Called with match-data from the field-name matcher while it's still
+active, i.e. `(match-string 1)' is the field name, and `(match-beginning
+0)' is the start of that field's line.
+
+Caveats:
+The return value needs to meet the following conditions:
+* it should always return a real, forward paragraph-end bound, not nil;
+* it should be strictly greater than (point).
+Otherwise, font lock will ignore the return value silently and fall back
+to search by the end of the current line."
+  (let* ((field (match-string-no-properties 1))
+         (field-beg (match-beginning 0))
+         (para-end (cdr (debian-rfc822-mode--paragraph-bounds))))
+    (put-text-property field-beg para-end 'font-lock-multiline t)
+    (setq debian-rfc822-mode--conditional-field field)
+    para-end))
+
+(defun debian-rfc822-mode--anchor-matcher (limit field-map)
+  "Match sub-items based on field names, bounded by LIMIT.
+The current field name is accessed in
+`debian-rfc822-mode--conditional-field', which can be used to get the
+sub-items to highlight in FIELD-MAP."
+  (when-let* ((field debian-rfc822-mode--conditional-field)
+              (sub-items (gethash field field-map nil))
+              (sub-item-regexp (regexp-opt sub-items 'words)))
+    (re-search-forward sub-item-regexp limit t)))
+
 (defun debian-rfc822-mode--field-font-lock-spec (field-spec)
   "Build a font lock spec based on FIELD-SPEC.
 See debian-rfc822-mode-field-spec."
-  (let (result)
+  (let ((field-map (make-hash-table :test 'equal))
+        fields)
     (dolist (field field-spec)
       (let* ((field-name (car field))
-             (sub-items (cdr field))
-             (field-name-regexp (concat "^" field-name ":"))
-             (sub-items-spec
-              (when sub-items
-                (let ((sub-item-regexp
-                       (concat "\\_<\\(" (regexp-opt sub-items) "\\)\\_>")))
-                  `(,sub-item-regexp nil nil
-                                     (1 font-lock-type-face))))))
-        (if sub-items-spec
-            (cl-pushnew `(,field-name-regexp
-                          (0 font-lock-keyword-face)
-                          ,sub-items-spec)
-                        result)
-          (cl-pushnew `(,field-name-regexp . font-lock-keyword-face)
-                      result))))
-    result))
+             (sub-items (cdr field)))
+        (cl-pushnew field-name fields)
+        (when sub-items
+          (puthash field-name sub-items field-map))))
+
+    (let* ((anchor-matcher (lambda (limit)
+                             (debian-rfc822-mode--anchor-matcher
+                              limit field-map))))
+      `(("^\\([[:alpha:]][[:alnum:]-]*\\):"
+         (1 (when (member (match-string-no-properties 1)
+                          (list ,@fields))
+              font-lock-keyword-face))
+         (,anchor-matcher
+          (debian-rfc822-mode--pre-match-form)
+          nil
+          (0 font-lock-type-face t)))))))
 
 (defun debian-rfc822-mode--variable-font-lock-spec (variable-spec)
   "Build a font lock spec for `@variable@' based on VARIABLE-SPEC.
@@ -164,7 +244,11 @@ MORE-SPEC can be used to specify additional highlighting customization."
                 ;; case-fold
                 nil
                 ;; syntax-alist
-                nil)))
+                nil))
+  (setq-local font-lock-multiline t)
+  (add-hook 'font-lock-extend-region-functions
+            #'debian-rfc822-mode--highlight-extend-region t t)
+  (font-lock-mode 1))
 
 (define-derived-mode debian-rfc822-mode text-mode "Debian rfc822-style mode"
   "Mode for rfc822-style Debian configuration files.
